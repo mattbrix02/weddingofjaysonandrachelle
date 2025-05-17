39ac22f3b58c8aa553be5f4cfceeda8b692cb8e5
mattbrix02 <mattbrix55@gmail.com>
2025-05-17 17:26:31 +0800

    new file:   resources/js/Layouts/MainLayout.vue 	modified:   resources/js/Pages/index.vue

--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>test</div>
+  <MainLayout>
+    <section>
+      test
+    </section>
+  </MainLayout>
 </template>
 
 
 
 <script setup>
+import MainLayout from '@/Layouts/MainLayout.vue'
+
 
 </script>
